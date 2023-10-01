f8bab0e583befde321d7ebdc80e8dd9a8fa79463
Maxime Petit <max57777@hotmail.fr>
2023-10-01 10:55:35 +0200

    ajout des bullets

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,7 @@ const slides = [
 const leftArrow = document.querySelector(".arrow_left");
 	console.log(leftArrow)
 
-	leftArrow.addEventListener ("click", () => {
+	leftArrow.addEventListener ("click", () => { 
 		console.log("j'ai cliqué sur la flèche gauche")
 })
 
@@ -33,3 +33,19 @@ const rightArrow = document.querySelector(".arrow_right");
 	rightArrow.addEventListener ("click", () => {
 		console.log("j'ai cliqué sur la flèche droite")
 })
+
+// ajout des bullets
+
+		// Récupération de la div parent
+const ajoutBullets = document.querySelector(".dots")
+
+for (let i = 0 ; i < slides.length; i++ ){
+
+	const bullet = document.createElement("p") //Création d'un point enfant
+	ajoutBullets.appendChild(bullet)//ajout a l'élement parent
+	bullet.classList.add("dot") // ajout d'une class
+	 
+	if (i === 3){
+		bullet.classList.add("dot_selected") //afficher le point blanc sur le bon bullet + encore modifier par la suite la valeur i
+	}
+}
